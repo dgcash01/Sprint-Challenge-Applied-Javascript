881d72fe41e903349d79a502fc711bac25dcf302
Dan Cashion <dangcashion@gmail.com>
2019-12-06 12:51:18 -0600

    Stretch Goal Attempt

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,29 @@
     <div class="right-button"> > </div>
   </div>
 */
+function carouselBuilder(picArray){
+    const myCarousel = document.createElement('div');
+    myCarousel.classList.add('carousel');
+    const myLeftBtn = document.createElement('div');
+    myLeftBtn.classList.add('left-button');
+    myLeftBtn.innerText = ' < '
+    myCarousel.appendChild(myLeftBtn);
+    picArray.forEach(element => {        
+      const myImage = document.createElement('img');
+      myImage.setAttribute('src', element);
+      myCarousel.appendChild(myImage);
+    });
+    const myRightBtn = document.createElement('div');
+    myRightBtn.classList.add('right-button');
+    myRightBtn.innerText = ' > '
+    myCarousel.appendChild(myRightBtn);
+    document.querySelector('.carousel-container').appendChild(myCarousel);
+}
+carouselBuilder(["./assets/carousel/mountains.jpeg","./assets/carousel/computer.jpeg","./assets/carousel/trees.jpeg","./assets/carousel/turntable.jpeg"]);
+
+document.querySelector('.carousel img').style.display = 'flex';
+document.addEventListener('DOMContentLoaded',function(event){
+  debugger;
+  document.querySelector('.carousel img:first').style('dislay: flex');
+});
+
