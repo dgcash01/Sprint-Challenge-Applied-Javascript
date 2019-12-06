6fd671b9e30f692853e1b2a376b8103ae81c3680
Dan Cashion <dangcashion@gmail.com>
2019-12-06 11:55:44 -0600

    MVP Complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,23 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const eHeader = document.createElement('div');
+    eHeader.classList.add ('header');
+    const eSpan = document.createElement('span');
+    eSpan.classList.add ('date');
+    eSpan.textContent = 'SMARCH 28, 2019';
+    const eHone = document.createElement('h1');
+    eHone.textContent = 'Lambda Times';    
+    const eTemp = document.createElement('span');
+    eTemp.classList.add ('temp');
+    eTemp.textContent = '98°';
+    eHeader.appendChild(eSpan);
+    eHeader.appendChild(eHone);
+    eHeader.appendChild(eTemp);
+
+    document.querySelector('.header-container').appendChild(eHeader);
+}
+Header ();
+
+
